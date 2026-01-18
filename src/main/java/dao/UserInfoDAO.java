be983317be5d14cb5a63130e0a3d642b6ab88515
ksydata <ksydata01@gmail.com>
2026-01-18 10:08:53 +0900

    modify UserInfoService matching UserInfoDAO, PagingService, RoleEnum

--- src/main/java/dao/UserInfoDAO.java
+++ src/main/java/dao/UserInfoDAO.java
@@ -14,6 +14,7 @@ import util.DatabaseUtil;
 
 // [AS-IS] 사용자 로그인 시 아이디로 받아온 학번/사번(userID)으로 DB에 접근하여 사용자 1명의 정보를 UserInfoDTO에 담아 반환
 public class UserInfoDAO {
+	// 1. 사용자의 본인 개인정보 조회(페이징 없음)
 	public List<UserInfoDTO> getMyInfo(String userID) {
 		List<UserInfoDTO> userInfoList = new ArrayList<>();
 		// 로그인 성공했을 때 사용자 개인정보를 받기 위한 빈 배열 객체 생성(초기화)		
@@ -21,9 +22,7 @@ public class UserInfoDAO {
 		
 		// 학번/사번(userID)으로 나의 개인정보 페이지에서 조회 또는 수정할 정보 불러오는 SQL 쿼리 
 		String userInfoSQL = """
-				SELECT 
-					U.userID, U.userPassword, U.name, U.phoneNumber, U.officeNumber, U.email,
-					P.college, P.major, P.status, P.residentNumber, P.address
+				SELECT U.*, P.*
 				FROM USER U 
 				LEFT JOIN PERSONAL_INFO P
 				ON U.userID = P.userID 
@@ -32,7 +31,7 @@ public class UserInfoDAO {
 		// String userInfoSQL = "SELECT * FROM USER WHERE userID = ?";
 		// String personalInfoSQL = "SELECT * FROM PERSONAL_INFO WHERE userID = ?";
     	
-    	// 1. USER 테이블에서 로그인 인증 관련 정보 추출 & PERSONAL_INFO 테이블에서 개인정보, 학사정보 추출
+    	// USER 테이블에서 로그인 인증 관련 정보 추출 & PERSONAL_INFO 테이블에서 개인정보, 학사정보 추출
 		try (Connection connection = DatabaseUtil.getConnection();
 			 PreparedStatement userInfoStatement = connection.prepareStatement(userInfoSQL)) {	
 			userInfoStatement.setString(1, userID);
@@ -51,45 +50,43 @@ public class UserInfoDAO {
     	return null;
 	}
 	
-	public List<UserInfoDTO> getTotalInfo(String userID) {
-		List<UserInfoDTO> userInfoList = new ArrayList<>();
-		// 로그인 성공했을 때 사용자 개인정보를 받기 위한 빈 배열 객체 생성(초기화)		
-		// UserInfoDTO userInfo = null;
+	// 2. 관리자의 전체 사용자 개인정보 조회(페이징 적용)
+	public List<UserInfoDTO> getTotalInfo(int limit, int offset) {
+		List<UserInfoDTO> totalInfoList = new ArrayList<>();
+		// 로그인 성공했을 때 전체 개인정보를 받기 위한 빈 배열 객체 생성(초기화)		
 		
-		// 학번/사번(userID)으로 나의 개인정보 페이지에서 조회 또는 수정할 정보 불러오는 SQL 쿼리 
-		String userInfoSQL = """
-				SELECT 
-					U.userID, U.userPassword, U.name, U.phoneNumber, U.officeNumber, U.email,
-					P.college, P.major, P.status, P.residentNumber, P.address
+		// 전체 개인정보 페이지에서 조회 또는 수정할 정보 불러오는 SQL 쿼리 
+		String totalInfoSQL = """
+				SELECT U.*, P.*
 				FROM USER U 
 				LEFT JOIN PERSONAL_INFO P
-				ON U.userID = P.userID 
-				WHERE U.userID = ?
+				ON U.userID = P.userID
+				ORDER BY userID ASC
+				LIMIT ? OFFSET ?
 		""";
-		// String userInfoSQL = "SELECT * FROM USER WHERE userID = ?";
-		// String personalInfoSQL = "SELECT * FROM PERSONAL_INFO WHERE userID = ?";
-    	
-    	// 1. USER 테이블에서 로그인 인증 관련 정보 추출 & PERSONAL_INFO 테이블에서 개인정보, 학사정보 추출
+		
+    	// USER 테이블에서 로그인 인증 관련 정보 추출 & PERSONAL_INFO 테이블에서 개인정보, 학사정보 추출
 		try (Connection connection = DatabaseUtil.getConnection();
-			 PreparedStatement userInfoStatement = connection.prepareStatement(userInfoSQL)) {	
-			userInfoStatement.setString(1, userID);
-			ResultSet resultSet = userInfoStatement.executeQuery();
+			 PreparedStatement totalInfoStatement = connection.prepareStatement(totalInfoSQL)) {	
+			totalInfoStatement.setInt(1, limit);
+			totalInfoStatement.setInt(2, offset);
+			ResultSet resultSet = totalInfoStatement.executeQuery();
 			// 쿼리(where절 userID = ?)에 사번 포함하여 쿼리 실행결과 담을 객체 생성
 			
 			if (resultSet.next()) {
 				UserInfoDTO userInfo = ROW_MAPPER.mapRow(resultSet);
-				userInfoList.add(userInfo);
+				totalInfoList.add(userInfo);
 	        } 	
 		} catch (Exception e) {
 			// 데이터베이스 오류 발생			
 			e.printStackTrace();
 		}
-    	// userID에 해당하는 사용자가 존재하지 않을 경우
+    	// 사용자 개인정보가 존재하지 않을 경우
     	return null;
 	}
 	
 	
-	// 2. Row-to-DTO 매핑을 분리하는 RowMapper 패턴
+	// 3. Row-to-DTO 매핑을 분리하는 RowMapper 패턴
     // DAO의 구조를 유지하면서 Service(Paging, UserInfo)에서 RowMapper 재사용	
 	public static final RowMapper<UserInfoDTO> ROW_MAPPER = resultSet -> {
 		UserInfoDTO userInfo = new UserInfoDTO();
@@ -115,6 +112,7 @@ public class UserInfoDAO {
 	};
 
 	
+	// 4. 사용자의 본인 개인정보 수정
 	// [TO-BE] 하나의 동적 쿼리로 변경된 필드만 업데이트(Java Persistence API [X] → Eclipse + JDBC)
 	public UserInfoDTO updateUserInfo(String userID, String phoneNumber, String officeNumber, 
             String email, String address) {
@@ -168,14 +166,10 @@ public class UserInfoDAO {
 						// for문 활용하여 파라미터 바인딩
 						updateStatement.setObject(
 								i+1, 
-						// ["010-1234-5678", "123", "user@univ.com", "2025001"]
 								parameters.get(i));
 						// UPDATE USER SET phoneNumber = ?, officeNumber = ?, email = ? WHERE userID = ?
-					} 
-
-					
-
-
+						// ["010-1234-5678", "123", "user@univ.com", "2025001"]
+					}
 					updateStatement.executeUpdate();
 					// 쿼리 실행
 				}
@@ -224,6 +218,7 @@ public class UserInfoDAO {
 	}
 	*/
 	
+	// 5. 사용자의 본인 비밀번호 변경 전 검증
 	public boolean checkCurrentPassword(String userID, String inputPassword) {
 		// 비밀번호 변경 전 현재 비밀번호에 대한 확인용 메서드
 		String checkQuery = "SELECT userPassword FROM USER WHERE userID = ?";
@@ -249,6 +244,7 @@ public class UserInfoDAO {
 		return false;
 	}	
 	
+	// 6. 사용자의 본인 비밀번호 변경
 	public UserInfoDTO updatePassword(String userID, String userPassword) {
 		// 학번/사번(userID)으로 나의 개인정보 페이지에서 조회되는 비밀번호를 변경하는 SQL 쿼리
 		String updateQuery = "UPDATE USER SET userPassword = ? WHERE userID = ?";
